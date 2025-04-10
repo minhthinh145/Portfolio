a4afa9d1fab9babe94f27fbddf9308168dc78db0
minhthinh145 <minhthinh0384360149@gmail.com>
2025-04-10 08:20:09 +0700

    fix href for linkedin and adjustment projects

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -11,15 +11,19 @@ export const Projects = () => {
 
                 Featured Projects
             </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-1 gap-6">
                 <div className="p-6 rounded-xl border border-white/10 
                 hover:-translate-y-1 hover:border-blue-500/30 
                 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
                 
                 ">
-                <h3 className="text-xl font-bold mb-2"> Website E-comerce</h3>
+                <h3 className="text-xl font-bold mb-2 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent"> Website E-comerce</h3>
                     <p className="text-gray-300 mb-4">
                         This project is a modern e-commerce website designed for selling high-quality keyboards. It features a sleek user interface, responsive design, and seamless navigation to provide an excellent shopping experience for keyboard enthusiasts.
+                    </p>
+                <h3 className="text-xl font-bold mb-2 bg-gradient-to-l from-blue-500 to-cyan-400 bg-clip-text text-transparent">In Progress</h3>
+                    <p className="text-gray-300 mb-4">
+                        The backend has been completed, and the frontend is currently under development.
                     </p>
                     <div className="w-full flex flex-wrap gap-2">
                         {["React","TailwindCSS","MSSQL" , "ASP .NET Core" , "AWS", "GIT", "Docker"].map((tech,key) => (
@@ -45,42 +49,7 @@ export const Projects = () => {
                             <img src={socialImage} alt="GitHub Logo" className="h-12 w-12 ml-2" />
                         </a>
                     </div>
-                </div>
-
-                <div className="p-6 rounded-xl border border-white/10 
-                hover:-translate-y-1 hover:border-blue-500/30 
-                hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
-                
-                ">
-                <h3 className="text-xl font-bold mb-2"> Website E-comerce</h3>
-                    <p className="text-gray-300 mb-4">
-                        This project is a modern e-commerce website designed for selling high-quality keyboards. It features a sleek user interface, responsive design, and seamless navigation to provide an excellent shopping experience for keyboard enthusiasts.
-                    </p>
-                    <div className="w-full flex flex-wrap gap-2">
-                        {["React" , "ASP .NET Core" , "AWS", "GIT", "Docker"].map((tech,key) => (
-                            <span 
-                            key ={key}
-                            className="bg-blue-500/10 text-blue-500 py-1 px-3 
-                            rounded-full text-sm hover:bg-blue-500/20
-                            hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
-                            ">
-                                {tech}
-                            </span>
-                        ))}
-                    </div>
-
-                    <div className="flex items-center gap-x-2 my-4">
-                        <a 
-                            href="https://github.com/minhthinh145/WebsiteSoldKeyBoard" 
-                            target="_blank" 
-                            rel="noopener noreferrer" 
-                            className="text-blue-400 hover:text-blue-300 transition-colors flex items-center"
-                        >
-                            View Project 
-                            <img src={socialImage} alt="GitHub Logo" className="h-12 w-12 ml-2" />
-                        </a>
-                    </div>
-                </div>
+                </div>  
             </div>
         </div>
         </RevealOnScroll>
